4379dab0fe8bb22e8efb957fec7c7eaf5ad8c128
timskeee <timskee@gmail.com>
2023-10-10 09:00:18 -0700

    fixed path issue with * in name

--- NeuronModelClass.py
+++ NeuronModelClass.py
@@ -12,7 +12,7 @@ import os
 import csv
 
 class NeuronModel:
-    def __init__(self, mod_dir = './Neuron_Model_HH/',#'./Neuron_Model_HH/', 
+    def __init__(self, mod_dir = './Neuron_Model_12HMM16HH/',#'./Neuron_Model_HH/', 
     
                       nav12=1,
                       nav16=1,
