96eefe1d52426955eb5a00fc1eae2dde5ec9d9ce
timskeee <timskee@gmail.com>
2024-07-15 11:33:24 -0700

    Added 12HH16HH plotting notebook for Roy

--- Na12HMMModel_TF.py
+++ Na12HMMModel_TF.py
@@ -594,9 +594,9 @@ class Na12Model_TF:
         ###_________________________WT following redistribution of na12/16 in AIS
         # wt_fi = [0, 0, 5, 9, 13, 16, 18, 20, 22, 23, 25, 26, 28, 29, 30, 31, 32, 33, 34, 35, 36] #na12_HMM_TF100923 WT w/rounded AIS, 7,7 of ais_na12/16, and 2na12, 4na16
         # wt_fi = [0,0,4,9,13,16,18,20,22,24,25,27,28,29,31,32,33,34,35,36,37] #na12_HMM_TF100923 WT w/rounded AIS, 7,7 of ais_na12/16, and 4na12, 3na16
-        # wt_fi = [0,0,6,9,11,12,13,15,16,17,19,20,21,22,23,23,31,32,33,34,34] ##TF040324 FI vals from 12HH16HH working model 040324
+        wt_fi = [0,0,6,9,11,12,13,15,16,17,19,20,21,22,23,23,31,32,33,34,34] ##TF040324 FI vals from 12HH16HH working model 040324
         # wt_fi = [0,0,0,2,7,8,9,10,11,12,13,13,14,14,15,16,16,17,17,18,18] ##TF052424 FI vals from 12HMM16HH working WT model
-        wt_fi = [0, 0, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11, 11, 12, 13, 13, 14, 15, 15, 16] ##TF062424 FI vals for 12HMM16HMM WT model
+        # wt_fi = [0, 0, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11, 11, 12, 13, 13, 14, 15, 15, 16] ##TF062424 FI vals for 12HMM16HMM WT model
         
 
         for curr_amp in vs_amp: #vs_amp is list
