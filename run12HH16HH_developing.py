8f611c3cacfb109d5c88fd6bdd64fb0d5d6b1338
timskeee <timskee@gmail.com>
2025-05-09 14:56:01 -0700

    Restarted dev model building with qa and param update fixes from Roy branch

--- run12HH16HH_developing.py
+++ run12HH16HH_developing.py
@@ -172,10 +172,10 @@ filenamemut = './params/na12annaTFHHmut.txt'
 
 
 ## 1.2HH params newest after Kevin's inpurt. 12-16 gap closer to 5mV now.
-changesna12={"Rd": 0.023204006298533603, "Rg": 0.015604498120126004, "Rb": 0.0925081211054913, "Ra": 0.23933332265451177, "a0s": 0.0005226303768198727, "gms": 0.14418575154491814, "hmin": 0.008449935591049326, "mmin": 0.01193016441163175, "qinf": 5.7593653647578105, "q10": 2.1532859986639186, "qg": 1.2968193480468215, "qd": 0.661199851452832, "qa1": 4.492758160759386, "smax": 3.5557932199839737, "sh": 8.358558450280716, "thinf": -47.8194205612529, "thi2": -79.6556083820085, "thi1": -62.40165437813537, "tha": -33.850064879126805, "vvs": 1.4255479951467982, "vvh": -55.33213046147061, "vhalfs": -40.89976480829731, "zetas": 13.403615755952343}
+changesna12={"Rd": 0.023204006298533603, "Rg": 0.015604498120126004, "Rb": 0.0925081211054913, "Ra": 0.23933332265451177, "a0s": 0.0005226303768198727, "gms": 0.14418575154491814, "hmin": 0.008449935591049326, "mmin": 0.01193016441163175, "qinf": 5.7593653647578105, "q10": 2.1532859986639186, "qg": 1.2968193480468215, "qd": 0.661199851452832, "qa": 5.41, "smax": 3.5557932199839737, "sh": 8.358558450280716, "thinf": -47.8194205612529, "thi2": -79.6556083820085, "thi1": -62.40165437813537, "tha": -33.850064879126805, "vvs": 1.4255479951467982, "vvh": -55.33213046147061, "vhalfs": -40.89976480829731, "zetas": 13.403615755952343}
 
 ## 16HH mod file params can be changed below
-changesna16 = {"Rd": 0.03, "Rg": 0.01, "Rb": 0.124, "Ra": 0.4, "a0s": 0.0003, "gms": 0.2, "hmin": 0.01, "mmin": 0.02, "qinf": 7, "q10": 2, "qg": 1.5, "qd": 0.5, "qa1": 7.2, "smax": 10, "sh": 8, "thinf": -51.5, "thi2": -47.5, "thi1": -47.5, "tha": -33.5, "vvs": 2, "vvh": -58, "vhalfs": -26.5, "zetas": 12}
+changesna16 = {"Rd": 0.03, "Rg": 0.01, "Rb": 0.124, "Ra": 0.4, "a0s": 0.0003, "gms": 0.2, "hmin": 0.01, "mmin": 0.02, "qinf": 7, "q10": 2, "qg": 1.5, "qd": 0.5, "qa": 7.2, "smax": 10, "sh": 8, "thinf": -51.5, "thi2": -47.5, "thi1": -47.5, "tha": -33.5, "vvs": 2, "vvh": -58, "vhalfs": -26.5, "zetas": 12}
 
 modify_dict_file(filename12, changesna12)
 modify_dict_file(filename16, changesna16)
@@ -193,36 +193,77 @@ config_dict2={"sim_config_nexus": sim_config_nexus,
 config_dict3={"sim_config_soma": sim_config_soma}
 
 for config_name, config in config_dict3.items():
-  path = f'cell_1/6-0.3gbar_channels'
+  path  = '3-cell_2'
+  path1 = '3-cell_2/dvdt'
+  path2 = '3-cell_2/currentscapes'
 
+  out_path1 = os.path.join(root_path_out, path1)
+  out_path2 = os.path.join(root_path_out, path2)
+
+  if not os.path.exists(out_path1):
+      os.makedirs(out_path1)
+  if not os.path.exists(out_path2):
+      os.makedirs(out_path2)
 # for morphology_index,morphoname in morphology_files.items():
 #   set_morphology(morphology_index)
 
 
 allmutsefel = pd.DataFrame()
 
-nav16factor=0.75 ## remove 1.6 in developing model when only 1.2 present. Params replaced by 12
-nav12factor=0.75 
+nav16factor=1 ## remove 1.6 in developing model when only 1.2 present. Params replaced by 12
+nav12factor=1 
 somaKfac=1
 kpfac=1
 ktfac=1
-aiscafac=1*50
+aiscafac=1
 aisKcafac=1
+f=1
 
-for aiscafac in [0.1,0.25,0.5,0.75]:
-  simwt = tf.Na12Model_TF(ais_nav12_fac=5.76*nav12factor,nav12=1.1*nav12factor,
-                          ais_nav16_fac=1.08*nav16factor,nav16=1.43*nav16factor,
-                          somaK=0.022, 
-                          KP=3.9375*kpfac, 
-                          KT=5*ktfac,
-                          ais_ca = 43*aiscafac,
-                          ais_Kca = 0.25*aisKcafac,
-                          soma_na16=0.8*nav16factor,soma_na12=2.56*nav12factor,
+'''
+sim1 = tf.Na12Model_TF(ais_nav12_fac=5.76*nav12factor*f,
+                            nav12=1.1*1.1*nav12factor*f,
+                            ais_nav16_fac=1.08*nav16factor*f,
+                            nav16=1.43*1.2*nav16factor*f,
+                            somaK=0.022*f, 
+                            KP=3.9375*kpfac*f, 
+                            KT=5*ktfac*f,
+                            ais_ca = 43*0.5*aiscafac*f,
+                            ais_Kca = 0.25*aisKcafac*f,
+                            soma_na16=0.8*nav16factor*f,
+                            soma_na12=2.56*nav12factor*f,
+                            node_na = 1,
+                            dend_nav12=1*f,
+                            na12name = 'na12annaTFHH2',mut_name = 'na12annaTFHH2',na12mechs = ['na12','na12mut'],
+                            na16name = 'na12annaTFHH2',na16mut_name = 'na12annaTFHH2',na16mechs=['na16','na16mut'],params_folder = './params/',
+                            plots_folder = f'{root_path_out}/{path}', update=True, fac=None)
+fig_volts,axs = plt.subplots(2,figsize=(cm_to_in(8),cm_to_in(15)))
+sim1.plot_stim(axs = axs[0],stim_amp = 0.3,dt=0.005, clr='cadetblue')
+plot_dvdt_from_volts(sim1.volt_soma, sim1.dt, axs[1],clr='cadetblue')
+fig_volts.savefig(f'{sim1.plot_folder}/dvdt/1-Adult_params.pdf')
+sim1.make_currentscape_plot(amp=0.5, time1=50,time2=150,stim_start=30, sweep_len=200, pfx=f'currentscapes/1-Adult_params_')
+'''
+
+for f in [0.001,0.01,0.1,0.25,0.5,0.75,1.25,1.5,2,5,10]:
+# for nav16factor in [0.75,1.25,1.5]:
+  simwt = tf.Na12Model_TF(ais_nav12_fac=5.76*nav12factor,
+                          nav12=1.1*1.1*nav12factor,
+                          ais_nav16_fac=1.08*nav16factor,
+                          nav16=1.43*1.2*nav16factor,
+                          somaK=0.022*f, 
+                          KP=3.9375*kpfac*f, 
+                          KT=5*ktfac*f,
+                          ais_ca = 43*0.5*aiscafac*f,
+                          ais_Kca = 0.25*aisKcafac*f,
+                          soma_na16=0.8*nav16factor,
+                          soma_na12=2.56*nav12factor,
                           node_na = 1,
                           dend_nav12=1,
                           na12name = 'na12annaTFHH2',mut_name = 'na12annaTFHH2',na12mechs = ['na12','na12mut'],
                           na16name = 'na12annaTFHH2',na16mut_name = 'na12annaTFHH2',na16mechs=['na16','na16mut'],params_folder = './params/',
                           plots_folder = f'{root_path_out}/{path}', update=True, fac=None)
+  
+
+  
   # wt_Vm1,_,wt_t1,_ = simwt.get_stim_raw_data(stim_amp = 0.5,dt=0.005,rec_extra=False,stim_dur=1000, sim_config = config) #stim_amp=0.5
   # wt_fi=simwt.plot_fi_curve_2line(wt_data=None,wt2_data=None,start=-0.4,end=1,nruns=140, fn=f'WT_FI', epochlabel='200ms')
 
@@ -231,20 +272,66 @@ for aiscafac in [0.1,0.25,0.5,0.75]:
   fig_volts,axs = plt.subplots(2,figsize=(cm_to_in(8),cm_to_in(15)))
   simwt.plot_stim(axs = axs[0],stim_amp = 0.3,dt=0.005, clr='cadetblue')
   plot_dvdt_from_volts(simwt.volt_soma, simwt.dt, axs[1],clr='cadetblue')
-  fig_volts.savefig(f'{simwt.plot_folder}/aiscafac-{aiscafac}.pdf') #Change output file path here 
+  fig_volts.savefig(f'{simwt.plot_folder}/dvdt/6-K&Ca-{f}.pdf') #Change output file path here 
 
-# features_wt = ef.get_features(sim=simwt, prefix=f'{root_path_out}/{path}/WT', mut_name='WT')
-# allmutsefel = allmutsefel.append(features_wt, ignore_index=True)
+  # features_wt = ef.get_features(sim=simwt, prefix=f'{root_path_out}/{path}/WT', mut_name='WT')
+  # allmutsefel = allmutsefel.append(features_wt, ignore_index=True)
 
-# simwt.make_currentscape_plot(amp=0.5, time1=50,time2=100,stim_start=30, sweep_len=200, pfx='WT')
+  simwt.make_currentscape_plot(amp=0.5, time1=50,time2=150,stim_start=30, sweep_len=200, pfx=f'currentscapes/6-K&Ca-{f}_')
 # simwt.plot_model_FI_Vs_dvdt(wt_Vm=wt_Vm1,wt_t=wt_t1,sim_config=sim_config_soma,vs_amp=[0.5], fnpre=f'WT')
+
+'''
+for f in [0.001,0.01,0.1,0.25,0.5,0.75,1.25,1.5,2,5,10]:
+
+  sim2 = tf.Na12Model_TF(ais_nav12_fac=5.76*nav12factor*f,
+                            nav12=1.1*1.1*nav12factor*f,
+                            ais_nav16_fac=1.08*nav16factor*f,
+                            nav16=1.43*1.2*nav16factor*f,
+                            somaK=0.022*f, 
+                            KP=3.9375*kpfac*f, 
+                            KT=5*ktfac*f,
+                            ais_ca = 43*0.5*aiscafac*f,
+                            ais_Kca = 0.25*aisKcafac*f,
+                            soma_na16=0.8*nav16factor*f,
+                            soma_na12=2.56*nav12factor*f,
+                            node_na = 1,
+                            dend_nav12=1*f,
+                            na12name = 'na12annaTFHH2',mut_name = 'na12annaTFHH2',na12mechs = ['na12','na12mut'],
+                            na16name = 'na12annaTFHH2',na16mut_name = 'na12annaTFHH2',na16mechs=['na16','na16mut'],params_folder = './params/',
+                            plots_folder = f'{root_path_out}/{path}', update=True, fac=None)
+  fig_volts,axs = plt.subplots(2,figsize=(cm_to_in(8),cm_to_in(15)))
+  sim2.plot_stim(axs = axs[0],stim_amp = 0.3,dt=0.005, clr='cadetblue')
+  plot_dvdt_from_volts(sim2.volt_soma, sim2.dt, axs[1],clr='cadetblue')
+  fig_volts.savefig(f'{sim2.plot_folder}/dvdt/3-na16-{nav16factor}.pdf')
+  sim2.make_currentscape_plot(amp=0.5, time1=50,time2=150,stim_start=30, sweep_len=200, pfx=f'currentscapes/3-na16-{nav16factor}_')
+
+    
+  sim3 = tf.Na12Model_TF(ais_nav12_fac=5.76*nav16factor*f,
+                            nav12=1.1*1.1*nav16factor*f,
+                            ais_nav16_fac=1.08*nav16factor*f,
+                            nav16=1.43*1.2*nav16factor*f,
+                            somaK=0.022*f, 
+                            KP=3.9375*kpfac*f, 
+                            KT=5*ktfac*f,
+                            ais_ca = 43*0.5*aiscafac*f,
+                            ais_Kca = 0.25*aisKcafac*f,
+                            soma_na16=0.8*nav16factor*f,
+                            soma_na12=2.56*nav16factor*f,
+                            node_na = 1,
+                            dend_nav12=1*f,
+                            na12name = 'na12annaTFHH2',mut_name = 'na12annaTFHH2',na12mechs = ['na12','na12mut'],
+                            na16name = 'na12annaTFHH2',na16mut_name = 'na12annaTFHH2',na16mechs=['na16','na16mut'],params_folder = './params/',
+                            plots_folder = f'{root_path_out}/{path}', update=True, fac=None)
+  fig_volts,axs = plt.subplots(2,figsize=(cm_to_in(8),cm_to_in(15)))
+  sim3.plot_stim(axs = axs[0],stim_amp = 0.3,dt=0.005, clr='cadetblue')
+  plot_dvdt_from_volts(sim3.volt_soma, sim3.dt, axs[1],clr='cadetblue')
+  fig_volts.savefig(f'{sim3.plot_folder}/dvdt/4-na1216-{nav16factor}.pdf')
+  sim3.make_currentscape_plot(amp=0.5, time1=50,time2=150,stim_start=30, sweep_len=200, pfx=f'currentscapes/4-na1216-{nav16factor}_')
+  
+'''
 
 
 # NeuronModel.chandensities(name = f'{root_path_out}/densities_WT') ##TF uncomment to run function and plot channel densities in axon[0]
-
-# migraine={"F257I":{"Rd": 0.028485318424710374, "Rg": 0.018420814026814298, "Rb": 0.05777962337521357, "Ra": 0.21341303335597034, "a0s": 0.00046381351100748496, "gms": 0.1341164855099387, "hmin": 0.006864146828635849, "mmin": 0.007621368015226187, "qinf": 6.486459886532015, "q10": 2.561051746368186, "qg": 1.3943995618682714, "qd": 0.797072687262635, "qa1": 6.540179485338942, "smax": 5.751234160342341, "sh": 9.898965372911075, "thinf": -42.299802571430824, "thi2": -64.0747495998566, "thi1": -48.0852551990378, "tha": -30.139992632180743, "vvs": 0.8468560048684934, "vvh": -53.739541326283735, "vhalfs": -42.22939001512733, "zetas": 12.811495608869796},
-# "R1319G":{"Rd": 0.01821017957894782, "Rg": 0.015835927879902578, "Rb": 0.08669778691261865, "Ra": 0.25222138069588795, "a0s": 0.0005998602174497901, "gms": 0.15734865667490094, "hmin": 0.003049022325449447, "mmin": 0.009227432163769297, "qinf": 7.542037850663156, "q10": 1.3991326492121134, "qg": 0.11658354700541385, "qd": 0.41022807889968216, "qa1": 6.23800810747929, "smax": 2.297618044174789, "sh": 9.225725362157418, "thinf": -46.09023665760313, "thi2": -57.0320710747067, "thi1": -62.70708396987019, "tha": -29.27378295119758, "vvs": 0.25717057711770985, "vvh": -58.74304862555019, "vhalfs": -31.258170795759927, "zetas": 10.786789360357215},
-#  "K1480E":{"Rd": 0.017988222179818, "Rg": 0.014526835880098123, "Rb": 0.032287886870504066, "Ra": 0.20374071568963156, "a0s": 0.00043332968046547453, "gms": 0.2506541588105391, "hmin": 0.003724133667885614, "mmin": 0.022703936878165614, "qinf": 8.056557948665372, "q10": 2.1947359595099254, "qg": 1.607903072936985, "qd": 0.9369754615041643, "qa1": 6.825173338201797, "smax": 9.756858258324922, "sh": 9.29652730203526, "thinf": -44.454878232047506, "thi2": -78.8896576923306, "thi1": -61.97876376872753, "tha": -25.286365983599477, "vvs": 0.665802756552117, "vvh": -45.45240491205891, "vhalfs": -18.50057219270583, "zetas": 11.579233310014331},}
 
 # # e9 ={"E999K":{"Rd": 0.026143636968066552, "Rg": 0.01668223028930355, "Rb": 0.014291884348195151, "Ra": 0.3173306743768715, "a0s": 0.0005030781620195419, "gms": 0.2257551972957173, "hmin": 0.01486999595016371, "mmin": 0.01069596553673631, "qinf": 4.543449675893968, "q10": 2.905858138508142, "qg": 1.070567987818106, "qd": 0.8859991427475545, "qa1": 2.7332610992423385, "smax": 4.562540093135402, "sh": 8.697260358969865, "thinf": -40.098974160495985, "thi2": -79.86939611595125, "thi1": -59.571867025996255, "tha": -25.969320595959655, "vvs": 0.05603301531867544, "vvh": -47.92236852687354, "vhalfs": -24.29418118596604, "zetas": 11.472449828098517},
 # # }
@@ -316,32 +403,3 @@ for aiscafac in [0.1,0.25,0.5,0.75]:
 # "mut12_3":{"Rd": 0.026142058705206105, "Rg": 0.019866885290558384, "Rb": 0.061895058538106174, "Ra": 0.27009978929276024, "a0s": 0.0005283916660905223, "gms": 0.12181212025656826, "hmin": 0.012735392704874001, "mmin": 0.008771388550581054, "qinf": 6.045788367057046, "q10": 2.538082583168175, "qg": 0.9933320760339781, "qd": 0.8755090306886261, "qa1": 4.272296506754877, "smax": 0.48118157879662515, "sh": 6.337925231264073, "thinf": -47.92381574816813, "thi2": -68.77550985279592, "thi1": -54.514246938787124, "tha": -29.85423299386167, "vvs": 1.0183964850836311, "vvh": -57.26142719931541, "vhalfs": -27.44188335217487, "zetas": 14.714002514530646},
 # "mut12_4":{"Rd": 0.02848830957015073, "Rg": 0.017920288232627524, "Rb": 0.07584414429142777, "Ra": 0.25298184954858416, "a0s": 0.00024016222017944134, "gms": 0.056371552016566955, "hmin": 0.003939106112054973, "mmin": 0.007774416333231421, "qinf": 5.7381817490450375, "q10": 2.4069457527425504, "qg": 0.08895001762541088, "qd": 0.9936571912524743, "qa1": 4.7641269763842855, "smax": 13.443348110571176, "sh": 7.816544273685546, "thinf": -47.865012506888945, "thi2": -42.14702114139171, "thi1": -48.69279065412408, "tha": -31.189788720642483, "vvs": 1.5250529682538327, "vvh": -60.711370331574564, "vhalfs": -11.317823554253446, "zetas": 8.411574870909128},}
 
-
-
-
-# for mutname,dict in synthmuts.items():
-#   print(f"mutname is {mutname}")
-#   print(f"it's corresponding dictionary is {dict}")
-#   modify_dict_file(filenamemut,dict)
-# #   # modify_dict_file(filename16,dict)
-
-
-# # for factor in [0.00001,0.0001,0.001,0.01,0.1,0.25,0.5,0.75,1,1.2,2,4,6,10,25,50,100,1000,10000,100000]:
-# # for amps in np.arange(-0.4,0.4,0.05):
-# # for factor in [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]:
-
-# ##Mutant/Variant
-#   simmut = tf.Na12Model_TF(ais_nav12_fac=5.76,ais_nav16_fac=1.08,
-#                               nav12=1.1,nav16=1.43, somaK=0.022, KP=3.9375, KT=5,
-#                               ais_ca = 43,ais_Kca = 0.25,
-#                               soma_na16=0.8,soma_na12=2.56,node_na = 1,
-#                               dend_nav12=1,
-#                               na12name = 'na12annaTFHH2',mut_name = 'na12annaTFHH2mut',na12mechs = ['na12','na12mut'],
-#                               na16name = 'na16HH_TF2',na16mut_name = 'na16HH_TF2',na16mechs=['na16','na16mut'],params_folder = './params/',
-#                               plots_folder = f'{root_path_out}/{path}', update=True, fac=None)
-#   # simmut.wtvsmut_stim_dvdt(wt_Vm=wt_Vm1,wt_t=wt_t1,sim_config=sim_config_soma,vs_amp=[0.5],stim_dur=1700, fnpre=f'{mutname}-')
-#   features_df1 = ef.get_features(sim=simmut, prefix=f'{root_path_out}/{path}', mut_name = f'{mutname}')
-
-#   allmutsefel = allmutsefel.append(features_df1,ignore_index=True)
-# allmutsefel.to_csv(f'{root_path_out}/{path}/allsynthmuts_EFEL_111524b.csv')
-  
